d9fc9c809d4daa7350c92c55a242fe2a649fef15
amin roosta <amin.roosta@outlook.com>
2016-09-30 22:42:06 +0330

    remove extra codes in cpp example

--- cpp-example/main.cpp
+++ cpp-example/main.cpp
@@ -6,74 +6,8 @@ extern "C" {
 }
 using namespace std;
 
-sqlite3* db;
-
-int first_row;
-
-int select_callback(void *p_data, int num_fields, char **p_fields, char **p_col_names) {
-
-  int i;
-
-  int* nof_records = (int*) p_data;
-  (*nof_records)++;
-
-  if (first_row) {
-    first_row = 0;
-
-    for (i=0; i < num_fields; i++) {
-      printf("%20s", p_col_names[i]);
-    }
-
-    printf("\n");
-    for (i=0; i< num_fields*20; i++) {
-      printf("=");
-    }
-    printf("\n");
-  }
-
-  for(i=0; i < num_fields; i++) {
-    if (p_fields[i]) {
-      printf("%20s", p_fields[i]);
-    }
-    else {
-      printf("%20s", " ");
-    }
-  }
-
-  printf("\n");
-  return 0;
-}
-
-void print_rows() {
-  const char* query = "select * from user";
-  char *errmsg;
-  int   ret;
-  int   nrecs = 0;
-
-  first_row = 1;
-
-  ret = sqlite3_exec(db, query, select_callback, &nrecs, &errmsg);
-
-  if(ret!=SQLITE_OK) {
-    printf("Error in select statement %s [%s].\n", query, errmsg);
-  }
-  else {
-    printf("\n   %d records returned.\n", nrecs);
-  }
-}
-
-void sql_stmt(const char* stmt) {
-  char *errmsg;
-  int   ret;
-
-  ret = sqlite3_exec(db, stmt, 0, 0, &errmsg);
-
-  if(ret != SQLITE_OK) {
-    printf("Error in statement: %s [%s].\n", stmt, errmsg);
-  }
-}
-
 int main() {
+  sqlite3* db;
   sqlite3_open(":memory:", &db);
 
   const char * query = "create table if not exists user ("
