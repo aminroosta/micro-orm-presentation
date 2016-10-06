ed16a5868381cac5d9f21cb68d69aac6eef92076
amin roosta <amin.roosta@outlook.com>
2016-10-06 21:46:32 +0330

    cs example - lambda expressions

--- cs-example/Program.cs
+++ cs-example/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using System.Linq.Expressions;
 using System.Linq;
 using System.Collections.Generic;
 using Microsoft.Data.Sqlite;
@@ -75,6 +76,10 @@ namespace ConsoleApplication
             }
         }
         
+        public QueryBuilder<T> Query<T>() where T : new() {
+            var query = $"SELECT * from {typeof(T).Name} ";
+            return new QueryBuilder<T>(this, query);
+        }
           
         private Dictionary<string,string> _type_mappings = new Dictionary<string,string> {
             {"Int64","integer"},
@@ -99,8 +104,59 @@ namespace ConsoleApplication
         }
     }
     
-    public class Program
-    {
+    public class QueryBuilder<T>  where T: new() {
+        private string _query;
+        private DB _db;
+        public QueryBuilder(DB db, string query) {
+            _query = query;
+            _db = db;
+        }
+        private Dictionary<string,string> _method_mappings = new Dictionary<string,string> {
+            {"GreaterThan",">"},
+            {"LessThan","<"},
+            {"StartsWith","text"},
+            {"Double","real"},
+        };
+        public QueryBuilder<T> Where(Expression<Func<T,Object>> expr) {
+            string field = null;
+            string method = null;
+            string argument = null;
+            string condition = null;
+            if(expr.Body is UnaryExpression) {
+                var body = expr.Body as UnaryExpression;
+                if(body.Operand is BinaryExpression) {
+                    var operand = body.Operand as BinaryExpression;
+                    field = (operand.Left as MemberExpression).Member.Name;
+                    method = operand.NodeType.ToString();
+                    argument = (operand.Right as ConstantExpression).Value.ToString();
+                }
+                if (body.Operand is MethodCallExpression) {
+                   var operand = body.Operand as MethodCallExpression; 
+                   method = operand.Method.Name;
+                   field = (operand.Object as MemberExpression).Member.Name;
+                   argument = (operand.Arguments[0] as ConstantExpression).Value.ToString();
+                }
+               
+                if(field == null) {
+                    return this;
+                }
+                if(method == "GreaterThan") condition = $"{field} > {argument}";
+                if(method == "LessThan") condition = $"{field} < {argument}";
+                if(method == "StartsWith") condition = $"{field} LIKE '{argument}%'";
+                if(method == "EndsWith") condition = $"{field} LIKE '%{argument}'";
+                if(method == "Contains") condition = $"{field} LIKE '%{argument}%'";
+                
+                _query = $"{_query} WHERE {condition}";
+            }
+            return this;
+        }
+        
+        public IEnumerable<T> Run() {
+            return _db.Query<T>(_query);
+        }
+    }
+    
+    public class Program {
         public static void Main(string[] args) {
             var db = new DB(":memory:");
             db.CreateTableIfNotExists<User>();
@@ -113,9 +169,19 @@ namespace ConsoleApplication
             db.Execute(query, new { age = 25, name = "phoebe", weight = 45.0 });
             db.Execute(query, new { age = 26, name = "rachel", weight = 50.0 });
             
+            
             foreach (var user in db.Query<User>("Select * from User")) {
                 Console.WriteLine($"{user.Id} {user.Name} {user.Age} {user.Weight}");
             }
+            Console.WriteLine("-----------------------------------------------------");
+            foreach(var user in db.Query<User>().Where(u => u.Age > 23).Run()) {
+                Console.WriteLine($"{user.Id} {user.Name} {user.Age} {user.Weight}");
+            }
+            Console.WriteLine("-----------------------------------------------------");
+            foreach(var user in db.Query<User>().Where(u => u.Name.StartsWith("r")).Run()) {
+                Console.WriteLine($"{user.Id} {user.Name} {user.Age} {user.Weight}");
+            }
+            
         }
     }
 }
